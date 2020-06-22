c25bd238d0efa291cdc93e8703ec391126f8a781
fyan1fyan1 <57881075+fyan1fyan1@users.noreply.github.com>
2020-06-22 15:48:56 -0400

    Update index.js

--- express-demo/index.js
+++ express-demo/index.js
@@ -4,16 +4,19 @@ const port = 3000
 
 app.get('/', (req, res) => res.json(fnJsonMessage(req.query.n)))
 
+//add number request of n to get expression result: e.x. '/?n=1'
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
-
+//function create message displaying results
 function fnJsonMessage(input){
+    //transfer input into number value
     inputValue = Number(input);
     let result = getFnResult(inputValue);
     jsonMessage = 'at n = '+input+', f(n) = n+1 = '+result;
     return jsonMessage;
 }
 
+//function caculates f(n) = n+1
 function getFnResult(input){
     n = input+1.0;
     return n;
