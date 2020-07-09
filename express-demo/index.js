a4400398fb406c6f9d38804347b15fd7c721bde4
fyan1fyan1 <57881075+fyan1fyan1@users.noreply.github.com>
2020-07-09 13:53:35 -0400

    add gitignore

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,22 +1,17 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.json(fnJsonMessage(req.query.n)))
+app.get('/', (req, res) => {
+    const n = Number(req.query.n);
+    const result = getFnResult(n);
+    res.header({"Access-Control-Allow-Origin": "*"});
+    res.json({ result: result });
+});
 
-//add number request of n to get expression result: e.x. '/?n=1'
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
-//function create message displaying results
-function fnJsonMessage(input){
-    //transfer input into number value
-    inputValue = Number(input);
-    let result = getFnResult(inputValue);
-    jsonMessage = 'at n = '+input+', f(n) = n+1 = '+result;
-    return jsonMessage;
-}
 
-//function caculates f(n) = n+1
 function getFnResult(input){
     n = input+1.0;
     return n;
